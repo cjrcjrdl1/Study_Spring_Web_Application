583d3350fdce14b6d7c365a788598aa4b063fa2e
cjrcjrdl1 <cjrcjrdl1@naver.com>
2023-01-27 11:44:06 +0900

    상품 서비스와 리포지토리 개발

--- webproject/src/main/java/min/webproject/domain/item/Item.java
+++ webproject/src/main/java/min/webproject/domain/item/Item.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import min.webproject.domain.Category;
+import min.webproject.exception.NotEnoughStockException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,4 +26,16 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<Category>();
+
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
---
+++ webproject/src/main/java/min/webproject/service/ItemService.java
@@ -0,0 +1,30 @@
+package min.webproject.service;
+
+import lombok.RequiredArgsConstructor;
+import min.webproject.domain.item.Item;
+import min.webproject.repository.ItemRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    @Transactional
+    public void saveItem(Item item) {
+        itemRepository.save(item);
+    }
+
+    public Item findOne(Long itemId) {
+        return itemRepository.findOne(itemId);
+    }
+
+    public List<Item> findItems() {
+        return itemRepository.findAll();
+    }
+}
