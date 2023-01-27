5b91cf08d91d5109e06d0df8abcc4a1183578a16
cjrcjrdl1 <cjrcjrdl1@naver.com>
2023-01-27 10:32:27 +0900

    회원 서비스 개발

---
+++ webproject/src/main/java/min/webproject/service/MemberService.java
@@ -0,0 +1,39 @@
+package min.webproject.service;
+
+import lombok.RequiredArgsConstructor;
+import min.webproject.domain.Member;
+import min.webproject.repository.MemberRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    @Transactional
+    public Long join(Member member) {
+        validDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+}
