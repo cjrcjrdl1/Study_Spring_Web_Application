670aa8a815f9a25b8073c158ec6006a0f2525b94
cjrcjrdl1 <cjrcjrdl1@naver.com>
2023-01-27 14:48:30 +0900

    상품 목록

--- webproject/src/main/java/min/webproject/web/ItemController.java
+++ webproject/src/main/java/min/webproject/web/ItemController.java
@@ -2,11 +2,14 @@ package min.webproject.web;
 
 import lombok.RequiredArgsConstructor;
 import min.webproject.domain.item.Book;
+import min.webproject.domain.item.Item;
 import min.webproject.service.ItemService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -32,6 +35,12 @@ public class ItemController {
 
         itemService.saveItem(book);
         return "redirect:/items";
+    }
 
+    @GetMapping(value = "/items")
+    public String list(Model model) {
+        List<Item> items = itemService.findItems();
+        model.addAttribute("items", items);
+        return "items/itemList";
     }
 }
